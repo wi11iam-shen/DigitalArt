cb5d67713b94f7348a53e92f7a0187bf5fb8a7f8
Dominique-Legaspi <domi.legaspi1006@gmail.com>
2024-06-06 13:39:26 -0700

    fixed image scaling

--- app/src/main/java/com/example/digitalartspace/MainActivity.kt
+++ app/src/main/java/com/example/digitalartspace/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,6 +27,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -54,13 +59,19 @@ class MainActivity : ComponentActivity() {
 fun DigitalArtSpace(paintings: List<Painting>, modifier: Modifier = Modifier){
     var imageSource by remember { mutableStateOf(1)}
     Column (modifier = Modifier.fillMaxWidth()){
-        Image(painter = painterResource(paintings[imageSource].imageResourceid),
-            contentDescription = stringResource(paintings[imageSource].title),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(500.dp)
-                .padding(top = 30.dp),
-            contentScale = ContentScale.FillBounds)
+        Box(modifier = modifier.fillMaxWidth()
+            .padding(top = 30.dp, start = 10.dp, end = 10.dp)
+            ) {
+            Image(
+                painter = painterResource(paintings[imageSource].imageResourceid),
+                contentDescription = stringResource(paintings[imageSource].title),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(500.dp)
+                    .clip(RoundedCornerShape(20.dp)),
+                contentScale = ContentScale.Crop
+            )
+        }
         Spacer(modifier.height(30.dp))
         AuthorInfo(artpiece = stringResource(paintings[imageSource].title),
             author = stringResource(paintings[imageSource].description),
