849b6277b4d87e88b8df862af86791a809777ffe
Dominique-Legaspi <domi.legaspi1006@gmail.com>
2024-06-06 13:22:28 -0700

    formatting text

--- app/src/main/java/com/example/digitalartspace/MainActivity.kt
+++ app/src/main/java/com/example/digitalartspace/MainActivity.kt
@@ -22,12 +22,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.digitalartspace.ui.theme.DigitalArtspaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,24 +56,34 @@ fun DigitalArtSpace(paintings: List<Painting>, modifier: Modifier = Modifier){
     Column (modifier = Modifier.fillMaxWidth()){
         Image(painter = painterResource(paintings[imageSource].imageResourceid),
             contentDescription = stringResource(paintings[imageSource].title),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
                 .height(500.dp)
-                .padding(top =30.dp),
+                .padding(top = 30.dp),
             contentScale = ContentScale.FillBounds)
-        Spacer(modifier.height(20.dp))
+        Spacer(modifier.height(30.dp))
         AuthorInfo(artpiece = stringResource(paintings[imageSource].title),
             author = stringResource(paintings[imageSource].description),
             year = stringResource(paintings[imageSource].year))
-        Spacer(modifier.height(20.dp))
-        Row(){
+        Spacer(modifier.height(30.dp))
+        Row(modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center){
             Button(onClick = { imageSource = (imageSource-1)%paintings.size
                 if(imageSource < 0){
                     imageSource = paintings.size-1
                 }
 
-            }){ Text(text = stringResource(R.string.back_btn))}
+            }){ Text(
+                text = stringResource(R.string.back_btn),
+                fontSize = 24.sp)}
+            Spacer(modifier = modifier.padding(16.dp))
             Button(onClick = { imageSource = (imageSource+1)%paintings.size }) {
-                Text(text = stringResource(R.string.next_btn))}
+                Text(
+                    text = stringResource(R.string.next_btn),
+                    fontSize = 24.sp)}
             }
     }
 }
@@ -81,9 +94,15 @@ fun DigitalArtSpace(paintings: List<Painting>, modifier: Modifier = Modifier){
 fun AuthorInfo(artpiece: String, author: String, year: String, modifier: Modifier=Modifier){
     val temp_string = author + " (" + year + ")"
     Box(modifier = Modifier){
-        Column(){
-            Text(text = artpiece)
-            Text(text = temp_string)
+        Column(modifier = modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally){
+            Text(text = artpiece,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold)
+            Text(text = temp_string,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.SemiBold)
         }
     }
 }
