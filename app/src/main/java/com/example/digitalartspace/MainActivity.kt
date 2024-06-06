6b38df0b4b0d21e4316c652bf864ca042680d4ba
William Shen <william.shen.0001@gmail.com>
2024-06-06 12:01:16 -0700

    Added image files, description, year, and image data source

--- app/src/main/java/com/example/digitalartspace/MainActivity.kt
+++ app/src/main/java/com/example/digitalartspace/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.digitalartspace.ui.theme.DigitalArtspaceTheme
 
@@ -23,21 +31,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             DigitalArtspaceTheme {
                 Surface {
-
+                    val paintings = DataSource().loadPainting()
+                    DigitalArtSpace(paintings)
                 }
             }
         }
     }
 }
 
+
+
 @Composable
-fun DigitalArtSpace(){
+fun DigitalArtSpace(paintings: List<Painting>, modifier: Modifier = Modifier){
+    var imageSource by remember { mutableStateOf(1)}
+    Column {
+        Image(painter = painterResource(paintings[imageSource].imageResourceid),
+            contentDescription = stringResource(paintings[imageSource].description))
 
+    }
 }
 
-fun Buttons(paintings: List<Painting>, modifier: Modifier=Modifier){
-    val total = paintings.size
-}
+
 
 @Composable
 fun AuthorInfo(artpiece: String, author: String, year: Int, modifier: Modifier=Modifier){
