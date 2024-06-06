e2941b0d2530106164c3071b15083ebed87bd261
KyleKekawa <104484279+KyleKekawa@users.noreply.github.com>
2024-06-06 14:30:04 -0700

    Centered the back and next

--- app/src/main/java/com/example/digitalartspace/MainActivity.kt
+++ app/src/main/java/com/example/digitalartspace/MainActivity.kt
@@ -1,53 +1,35 @@
 package com.example.digitalartspace
 
-
-/*
-Lab 2 Project
-
-Created By: William Shen, Dominique Legaspi, Kyle Kekawa
-
-William: Created foundation, buttons, and simple layout, as well as composables
-Dom: Created formatting, color, and image cropping
-Kyle: Created XML files
- */
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.digitalartspace.ui.theme.DigitalArtspaceTheme
+import kotlin.math.abs
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,69 +49,51 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun DigitalArtSpace(paintings: List<Painting>, modifier: Modifier = Modifier){
-    var imageSource by remember { mutableStateOf(1)}
-    Column (modifier = Modifier.fillMaxWidth()){
-        Box(modifier = modifier.fillMaxWidth()
-            .padding(top = 30.dp, start = 10.dp, end = 10.dp)
-            ) {
-            Box(modifier = Modifier.fillMaxWidth()
-                .height(500.dp)
-                .background(color = Color.LightGray)
-                .border(width = 2.dp, color = Color.Black)){
-                Image(
-                    painter = painterResource(paintings[imageSource].imageResourceid),
-                    contentDescription = stringResource(paintings[imageSource].title),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(500.dp)
-                        .clip(RoundedCornerShape(20.dp)),
-                    contentScale = ContentScale.Fit
-                )
+fun DigitalArtSpace(paintings: List<Painting>, modifier: Modifier = Modifier) {
+    var imageSource by remember { mutableIntStateOf(1) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .then(modifier),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(paintings[imageSource].imageResourceid),
+            contentDescription = stringResource(paintings[imageSource].description)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = {
+                imageSource = (imageSource - 1) % paintings.size
+                if (imageSource < 0) {
+                    imageSource = paintings.size - 1
+                }
+            }) {
+                Text(text = stringResource(R.string.back_btn))
+            }
+            Button(onClick = {
+                imageSource = (imageSource + 1) % paintings.size
+            }) {
+                Text(text = stringResource(R.string.next_btn))
             }
         }
-        Spacer(modifier.height(30.dp))
-        AuthorInfo(artpiece = stringResource(paintings[imageSource].title),
-            author = stringResource(paintings[imageSource].description),
-            year = stringResource(paintings[imageSource].year))
-        Spacer(modifier.height(30.dp))
-        Row(modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center){
-            Button(onClick = { imageSource = (imageSource-1)%paintings.size
-                if(imageSource < 0){
-                    imageSource = paintings.size-1
-                }
-
-            }){ Text(
-                text = stringResource(R.string.back_btn),
-                fontSize = 24.sp)}
-            Spacer(modifier = modifier.padding(16.dp))
-            Button(onClick = { imageSource = (imageSource+1)%paintings.size }) {
-                Text(
-                    text = stringResource(R.string.next_btn),
-                    fontSize = 24.sp)}
-            }
     }
 }
 
 
 
+
 @Composable
-fun AuthorInfo(artpiece: String, author: String, year: String, modifier: Modifier=Modifier){
+fun AuthorInfo(artpiece: String, author: String, year: Int, modifier: Modifier=Modifier){
     val temp_string = author + " (" + year + ")"
     Box(modifier = Modifier){
-        Column(modifier = modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally){
-            Text(text = artpiece,
-                fontSize = 32.sp,
-                fontWeight = FontWeight.Bold)
-            Text(text = temp_string,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.SemiBold)
+        Column(){
+            Text(text = artpiece)
+            Text(text = temp_string)
         }
     }
 }
@@ -139,6 +103,6 @@ fun AuthorInfo(artpiece: String, author: String, year: String, modifier: Modifie
 @Composable
 fun Preview() {
     DigitalArtspaceTheme {
-        AuthorInfo("Test", "Test", "5555")
+        AuthorInfo("Test", "Test", 5555)
     }
 }
